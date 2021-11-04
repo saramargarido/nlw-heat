b6ab007d11ff2b8ca97d7cf29461ac566a2392bd
saramargarido <saramargarido@gmail.com>
2021-11-04 11:05:23 -0300

    componente botão com icone

---
+++ nlwheatapp/src/components/Button/styles.ts
@@ -0,0 +1,20 @@
+import { StyleSheet } from 'react-native';
+import { FONTS } from '../../theme';
+
+export const styles = StyleSheet.create({
+  button: {
+      height: 48,
+      width: '100%',
+      flexDirection: 'row',
+      alignItems: 'center',
+      justifyContent: 'center',
+  },
+  title: {
+    textTransform: 'uppercase',
+    fontFamily: FONTS.BOLD,
+    fontSize: 14
+  },
+  icon: {
+      marginRight: 12
+  }
+});
